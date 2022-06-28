90e01668e749b86610454b7760322a835608ab56
Vitaly <brynzav2@gmail.com>
2022-06-28 12:05:05 +0300

    Начало создания  Архитектуры в node Type-Script

---
+++ users.ts
@@ -0,0 +1,20 @@
+ 
+ import express from 'express';
+ const userRouter = express.Router()
+
+
+  userRouter.use((req,res,next) => {
+ console.log('Обработчик users' )
+ next()
+  })
+
+ userRouter.post('/Login',(req,res) => {
+ res.send(' Hellow  Login')
+ })
+
+ userRouter.post('/Registr',(req,res) => {
+ res.send('Hellow Registr')
+ })
+
+export{userRouter}
+
